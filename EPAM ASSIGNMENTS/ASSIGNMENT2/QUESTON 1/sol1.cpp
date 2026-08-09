817880a1fa096c9b2178727dbf00825d22f57a64
vivanshrathore <vivanshrathore57@gmail.com>
2026-08-09 21:26:00 +0530

    Update sol1.cpp

--- EPAM ASSIGNMENTS/ASSIGNMENT2/QUESTON 1/sol1.cpp
+++ EPAM ASSIGNMENTS/ASSIGNMENT2/QUESTON 1/sol1.cpp
@@ -1,53 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int node, int parent, long long xorr, long long K,
-         long long &count, vector<long long> &keys,
-         vector<vector<int>> &adj) {
-
-    // Check current node
-    if (xorr >= K)
-        count++;
-
-    // Visit all adjacent nodes
-    for (int ngb : adj[node]) {
-        if (ngb != parent) {
-            
-            dfs(ngb, node, xorr ^ keys[ngb],
-                K, count, keys, adj);
-        }
-    }
+void dfs(int node, int parent, int xorr, int& k, int& count, 
+    vector<int>& keys, vector<vector<int>>& adj) {
+        if(xorr >= k) count++;
+        for(int& ngb : adj[node])
+            if(ngb != parent)
+                dfs(ngb, node, xorr^keys[ngb], k, count, keys, adj);
 }
 
 int main() {
-    int N;
-    long long K;
-
-    cin >> N >> K;
-
-    vector<long long> keys(N + 1);
-    vector<vector<int>> adj(N + 1);
-
-    // Input security keys
-    for (int i = 1; i <= N; i++) {
-        cin >> keys[i];
+    int N, k, count = 0; cin>>N>>k;
+    vector<int> keys(N+1, 0); 
+    vector<vector<int>> adj(N+1);
+    for(int i=1; i<=N; i++) cin>>keys[i];
+    for(int i=1; i<=N-1; i++) {
+        int src, dst; cin>>src>>dst;
+        adj[src].push_back(dst);
+        adj[dst].push_back(src);
     }
-
-    // Input tree edges
-    for (int i = 0; i < N - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    long long count = 0;
-
-    // Root is node 1, so start XOR with keys[1]
-    dfs(1, -1, keys[1], K, count, keys, adj);
-
+    dfs(1, -1, 0, k, count, keys, adj);
     cout << count << endl;
-
     return 0;
 }
