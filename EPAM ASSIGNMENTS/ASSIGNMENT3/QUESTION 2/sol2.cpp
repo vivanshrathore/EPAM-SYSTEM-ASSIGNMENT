1b63641b906fd417cc03d4eca6bdb071a0a27b0d
vivanshrathore <vivanshrathore57@gmail.com>
2026-08-13 19:41:30 +0530

    Refactor solution to use dynamic programming approach

--- EPAM ASSIGNMENTS/ASSIGNMENT3/QUESTION 2/sol2.cpp
+++ EPAM ASSIGNMENTS/ASSIGNMENT3/QUESTION 2/sol2.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
-    int n, totalBudget, currSpent=0, maxStudents=0; cin>>n>>totalBudget;
-    vector<int> requirements(n);
-    for(int i=0; i<n; i++) cin>>requirements[i];
-    sort(requirements.begin(), requirements.end());
-    for(int i=0; i<n; i++) {
-        if(requirements[i]+currSpent > totalBudget) break;
-        currSpent += requirements[i];
-        maxStudents++;
-    } cout << maxStudents << endl;
+    int n; cin>>n; 
+    vector<ll> points(n, 0), dp(n, 0);
+    for(int i=0; i<n; i++) cin>>points[i];
+    dp[0] = points[0];
+    dp[1] = max(points[0], points[1]);
+    if(n == 1) {
+        cout << points[0];
+        return 0;
+    } if(n == 2) {
+        cout << max(points[0], points[1]);
+        return 0;
+    } for(int i=2; i<n; i++)
+        dp[i] = max(dp[i-1], dp[i-2]+points[i]);
+    cout << dp[n-1] << endl;
     return 0;
 }
