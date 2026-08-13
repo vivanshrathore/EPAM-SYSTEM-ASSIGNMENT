774ee01d613559fccf608d3197d154abb828ec3e
vivanshrathore <vivanshrathore57@gmail.com>
2026-08-13 19:37:07 +0530

    Update sol2.cpp

---
+++ EPAM ASSIGNMENTS/ASSIGNMENT3/QUESTION 2/sol2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, totalBudget, currSpent=0, maxStudents=0; cin>>n>>totalBudget;
+    vector<int> requirements(n);
+    for(int i=0; i<n; i++) cin>>requirements[i];
+    sort(requirements.begin(), requirements.end());
+    for(int i=0; i<n; i++) {
+        if(requirements[i]+currSpent > totalBudget) break;
+        currSpent += requirements[i];
+        maxStudents++;
+    } cout << maxStudents << endl;
+    return 0;
+}
